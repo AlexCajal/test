ba23ef3f51d1d2496c6235782d3a4842538f03a6
AlexCajal <240151@fppiramide.com>
2025-12-17 08:49:27 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="idx.css">
+</head>
+<body>
+    <h1>PHP</h1>
+    <?php
+    echo "<h2>Hola</h2>"
+    >
+</body>
+</html>
